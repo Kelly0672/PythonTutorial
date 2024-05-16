f147904845cded204ab1dfa2e6dd72794c5413e1
Kelly0672 <169105220+Kelly0672@users.noreply.github.com>
2024-05-16 13:47:24 -0400

    Add files via upload

---
+++ Module Assessment/Second module/main2.py
@@ -0,0 +1,16 @@
+first_number= input("Enter a random number") 
+second_number= input("Enter a second random number") 
+first_number = float(first_number)
+second_number = float(second_number)
+Sum = first_number + second_number
+Subtraction = first_number - second_number
+Subtraction2 = second_number - first_number
+Multiplication =  first_number * second_number
+Division = first_number / second_number
+Division2 = second_number / first_number
+print("The sum is:", round(Sum,2))
+print("Subtracting the second number from the first is :", round(Subtraction,2))
+print("Subtracting the first number from the second is :", round(Subtraction2,2))
+print("Multiplication is:", round(Multiplication,2))
+print ("Dividing the second number into the first is: ", round(Division,2))
+print("Dividing the first number into the second is: ", round(Division2,2))
