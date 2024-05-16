6506f8d2301b75b4a6536bf38fc8df912a0ba84d
Kelly0672 <169105220+Kelly0672@users.noreply.github.com>
2024-05-16 13:22:36 -0400

    Add files via upload

---
+++ Module Assessment/Second module/main.py
@@ -0,0 +1,6 @@
+start_year= input("Enter the year you were born:") 
+end_year= input("Enter the current year") 
+SY = int(start_year)
+EY = int(end_year)
+Age = EY - SY
+print("Your current age is:", Age)
